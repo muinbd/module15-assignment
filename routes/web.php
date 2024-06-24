a3bfdfefd9f74f9e10ce06f14bd04403a8ecd6fe
Muin Chowdhury <muin.bd@gmail.com>
2024-06-24 18:12:32 +0600

    first commit

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProfileController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/profile/{id}', [ProfileController::class, 'index']);
+
